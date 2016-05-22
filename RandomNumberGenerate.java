4ebf74db0a872297f4b7b0dba32b4ba4f9cf396e
DhivyaaMurali <dhivshan86@gmail.com>
2016-05-22 23:05:42 +0530

    Add files via upload

---
+++ RandomNumberGenerate.java
@@ -0,0 +1,21 @@
+package session3_assignment;
+
+public class RandomNumberGenerate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	        int N = 4;
+	 
+	        //Generates a random number
+	        double randomNumber = Math.random();
+	        int randomInt = (int)(N * randomNumber);
+	 
+	        //Displays the random number
+	        System.out.println(randomInt);
+	 
+	    }
+
+	}
+
+
